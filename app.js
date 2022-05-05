7c8609f715d9e290b199bd83c10a27f9a827e348
Elias <eliaskibret2013n2@gmail.com>
2022-05-05 13:39:54 +0300

    env file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 require('dotenv').config()
 //async error
-
+const connectDB=require('./db/connect')
 const notFoundMiddleware=require('./middleware/not-found')
 const errorMiddleware=require('./middleware/error-handler')
 
@@ -21,12 +21,11 @@ app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
 
-const port =process.env.PORT || 3000;
+const port = 3000;
 
 const start=async ()=>{
 try{
-  //connect to
-  // await connectDB(process.env.MONGO_URI)
+  await connectDB(process.env.MONGO_URI)
   app.listen(port,console.log(`server is listening on port ${port}`))
 }
 catch(err){
