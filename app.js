956ac7edccb27d20f90b29b5475dc10c0873aea6
Elias <eliaskibret2013n2@gmail.com>
2022-05-05 12:10:01 +0300

    middleware function added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-// require('dotenv').config()
+require('dotenv').config()
 //async error
 
 const notFoundMiddleware=require('./middleware/not-found')
@@ -17,15 +17,16 @@ app.get('/',(req,res)=>{
 
 //product route
 
-// app.use(notFoundMiddleware)
+app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
 
-const port =3000;
+const port =process.env.PORT || 3000;
 
 const start=async ()=>{
 try{
-  
+  //connect to
+  // await connectDB(process.env.MONGO_URI)
   app.listen(port,console.log(`server is listening on port ${port}`))
 }
 catch(err){
