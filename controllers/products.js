7012e94c854c47f9ac7fa73bbf53daee43269437
Elias <eliaskibret2013n2@gmail.com>
2022-05-07 15:12:36 +0300

    filter

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,7 @@ const Product=require('../models/product')
 const getAllProductsStatic=async(req,res)=>{
     const search='ab'
     const product=await Product.find({price:{$gt:30}})
-                                       .sort('name')
+                                       .sort('price')
                                        .select('name price')
                                        .limit(4)
                                        .skip(5)
@@ -12,7 +12,7 @@ const getAllProductsStatic=async(req,res)=>{
 }
 
 const getAllProducts=async(req,res)=>{
-    const {featured,company,name,sort,fields}=req.query;
+    const {featured,company,name,sort,fields,numericFilters}=req.query;
 const queryObject={}
 if(featured){
     queryObject.featured=featured==='true'?true:false
@@ -34,7 +34,28 @@ if(fields){
     console.log(FieldLists)
     result=result.select(FieldLists)     
 }
+if(numericFilters){
+    const operatorMap={
+        '>':'$gt',
+        '>=':'$gte',
+        '=':'$eq',
+        '<':'$lt',
+        '<=':'$lte'
+    }
+    const regEx=/\b(<|>|>=|=|<|<=)\b/g
+    let filters=numericFilters.replace(regEx,(match)=>`-${operatorMap[match]}-`)
+    const options=['price','rating']
 
+    console.log(filters.split(','))
+    filters=filters.split(',').forEach((item)=>{
+    
+        const [field ,operator,value]=item.split('-')
+        if(options.includes(field)){
+            queryObject[field]={[operator]:Number(value)}
+        }
+    })
+    console.log(queryObject)
+}
 const page =Number(req.query.page)||1;
 const limit=Number(req.query.limit) || 10
 const skip=(page-1)*limit;
