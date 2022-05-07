82bce9825aaa116cc441f6c0a1de9101d3f97dd3
Elias <eliaskibret2013n2@gmail.com>
2022-05-07 11:51:31 +0300

    select and sort

--- controllers/products.js
+++ controllers/products.js
@@ -3,18 +3,35 @@ const Product=require('../models/product')
 
 const getAllProductsStatic=async(req,res)=>{
     const search='ab'
-    const product=await Product.find({}).sort('-name price')
+    const product=await Product.find({}).select('id')
     res.status(200).json({product,nbHits:product.length})
 }
 
 const getAllProducts=async(req,res)=>{
-    const {featured}=req.query;
-console.log(typeof(featured))
+    const {featured,company,name,sort,fields}=req.query;
 const queryObject={}
 if(featured){
     queryObject.featured=featured==='true'?true:false
 }
-const product=await Product.find(queryObject)
+let  result=Product.find(queryObject)
+
+//sort
+if(sort){
+    
+const sortLists=sort.split(',').join(" ")
+console.log(sortLists)
+result=result.sort(sortLists)
+}
+else{
+    result=result.sort('createdAt')
+}
+if(fields){
+    const FieldLists=fields.split(',').join(" ")
+    console.log(FieldLists)
+    result=result.select(FieldLists)     
+}
+const product=await result
+
 
     res.status(200).json({product,nbHits:product.length})
 }
